7afd7637c3f0d1b7f57e2f69f1536e4860a4d630
Zion Corbanie <zioncorb@gmail.com>
2025-01-19 17:19:15 +0100

    User upload images

--- internal/handlers/patchuseredit.go
+++ internal/handlers/patchuseredit.go
@@ -2,12 +2,16 @@ package handlers
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"sjb_site/internal/middleware"
 	"sjb_site/internal/store"
 	"sjb_site/internal/templates"
 	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type PatchUserEditHandler struct {
@@ -32,6 +36,13 @@ func (h *PatchUserEditHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+    //parse form
+    err := r.ParseMultipartForm(10 << 20)
+    if err != nil {
+        http.Error(w, "Unable to parse form", http.StatusBadRequest)
+        return
+    }
+
 	email := r.FormValue("email")
 	address := r.FormValue("address")
 	phone := r.FormValue("phone")
@@ -43,7 +54,35 @@ func (h *PatchUserEditHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 		PhoneNumber: phone,
 	}
 
-	err := h.userStore.PatchUser(userPatch)
+    //handle file upload
+    file, header, err := r.FormFile("image")
+    if err == nil {
+        defer file.Close()
+
+        fileName := fmt.Sprint("%d%s",userId,filepath.Ext(header.Filename))
+        uploadDir := "static/uploads/user"
+        os.MkdirAll(uploadDir, os.ModePerm)
+        filePath := filepath.Join(uploadDir, fileName)
+
+        dst, err := os.Create(filePath)
+        if err != nil {
+            http.Error(w, "Unable to save file", http.StatusInternalServerError)
+            return
+        }
+        defer dst.Close()
+
+        _, err = io.Copy(dst, file)
+        if err != nil {
+            http.Error(w, "Error saving file", http.StatusInternalServerError)
+            return
+        }
+
+        userPatch.Image = "/"+filePath
+        fmt.Printf("File uploaded successfully: %s\n", filePath)
+    }
+
+
+	err = h.userStore.PatchUser(userPatch)
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
