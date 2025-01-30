0fa6702572ec59db5f311f938d9ad9333ef28174
Zion Corbanie <zioncorb@gmail.com>
2025-01-30 16:34:21 +0100

    kleine fix

--- internal/handlers/postusermanagement.go
+++ internal/handlers/postusermanagement.go
@@ -23,7 +23,7 @@ func NewPostUserManagementHandler(params PostUserManagementHandlerParams) *PostU
 func (h *PostUserManagementHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	search := r.FormValue("search")
 	users, err := h.userStore.SearchUsers(search)
-	users = users[:10] // Alleen eerste 10 users getten
+	users = users[:min(10, len(users))] // Alleen eerste 10 users getten
 
 	if err != nil {
 		return
